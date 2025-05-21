49edde0f243b3125cef25233ebb8172357abccd1
Kommula Deepak Reddy <22x01a05a1@nrcmec.org>
2025-05-21 14:46:17 +0530

    Add files via upload

---
+++ day7_1.java
@@ -0,0 +1,23 @@
+package nrec1;
+class Test1{
+	final void show() {
+		System.out.println("Hello!!!!");
+	}
+}
+
+public class day7_1 {
+
+	void show() {
+		int a=23;
+		System.out.println(a);
+		a=32;
+		System.out.println(a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         day7_1 d1=new day7_1();
+         d1.show();
+	}
+
+}
